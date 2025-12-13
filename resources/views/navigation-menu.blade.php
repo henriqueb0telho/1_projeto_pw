34f457b85d7393fc04d27188b43cc2df486d465c
Henrique B. Oliveira <77446473+henriqueb0telho@users.noreply.github.com>
2025-12-13 00:20:14 -0100

    feat: app design changes

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto text-gray-800 dark:text-gray-200" />
+                        <x-application-logo class="block h-9 w-auto text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
